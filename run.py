b3c800e6fc9ca8959a0dcd1cbe394ab1379131be
Diogo Yaguas <diogo.yaguas@gmail.com>
2021-06-09 16:40:20 +0100

    Debug

--- run.py
+++ run.py
@@ -35,7 +35,9 @@ def classify():
         classes = {idx: value for idx, value in enumerate(label_vectorizer.classes_)}
         y_predict_proba = clf.predict_proba(vectorizer.transform([text]))
         res = [dict(zip_longest(classes, probs)) for probs in y_predict_proba][0]
+        print(res)
         for key in res:
+            print(key)
             res[classes[key]] = res.pop(key)
 
         print(res)
