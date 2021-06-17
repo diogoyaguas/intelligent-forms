2ce7a08c68f6c6f2a66a823a1d29cbe5d51e8ca6
Diogo Yaguas <diogo.yaguas@gmail.com>
2021-06-17 16:37:33 +0100

    Changes

--- src/run.py
+++ src/run.py
@@ -49,7 +49,7 @@ def classify():
             classes = {idx: value for idx, value in enumerate(
                 label_vectorizer.classes_)}
             y_predict_proba = clf.predict_proba(vectorizer.transform([text]))
-            print(y_predict_proba)
+            print(type(y_predict_proba))
             res = [dict(zip_longest(classes, probs))
                 for probs in y_predict_proba][0]
             print(res)
